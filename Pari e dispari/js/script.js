e652a4cb8063c251eed2b26cb143f197ca13bec4
ItsKristian01 <kristianhana11@gmail.com>
2023-10-26 17:49:02 +0200

    add: function odd even exercise

--- Pari e dispari/js/script.js
+++ Pari e dispari/js/script.js
@@ -6,33 +6,59 @@ console.log(userNumber);
 cpuNumber = Math.floor(Math.random() * 5) + 1;
 console.log(cpuNumber);
 
-if (userNumber > 5) {
-    alert("Numero non valido");
-} else if (userNumber < 1) {
-    alert("Numero non valido");
-} 
-
 let sum = userNumber + cpuNumber;
 console.log(`Questa e' la somma ${sum}`);
 
-if (sum % 2 === 0) {
-    sum = "pari";
-    console.log("Il numero e' pari")
-} else {
-    sum = "dispari";
-    console.log("Il numero e' dispari")
-}
-
-if (userNumber === "pari" && sum === "pari" ) {
-    console.log("Hai vinto");
-} else if (userNumber === "dispari" && sum === "dispari" ) {
-    console.log("Hai vinto");
-} else {
-    console.log("Ha vinto il Computer");
-}
-
-
-
 // Program Logic
+validatorNumber(userNumber);
+oddEvenNumber(sum);
+winner(userNumber, sum);
 
-// Output
+/**
+ * Description placeholder.
+ *
+ * @param {number}
+ * @returns {string}
+ */
+function validatorNumber(userNumber) {
+  if (userNumber > 5) {
+    alert("Numero non valido");
+  } else if (userNumber < 1) {
+    alert("Numero non valido");
+  } 
+  return;
+}
+
+/**
+ * Description placeholder.
+ *
+ * @param {number}
+ * @returns {string}
+ */
+function oddEvenNumber(sum) {
+  if (sum % 2 === 0) {
+    sum = "pari";
+    console.log("Il numero e' pari");
+  } else {
+    sum = "dispari";
+    console.log("Il numero e' dispari");
+  }
+  return;
+}
+
+/**
+ * Description placeholder.
+ *
+ * @param {number}
+ * @returns {string}
+ */
+function winner(userNumber, sum) {
+  if (userNumber === "pari" && sum === "pari") {
+    console.log("Hai vinto");
+  } else if (userNumber === "dispari" && sum === "dispari") {
+    console.log("Hai vinto");
+  } else {
+    console.log("Ha vinto il Computer");
+  }
+  return;
+}
